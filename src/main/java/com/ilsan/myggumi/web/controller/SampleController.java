cbf9c52b8727b05f699e44e5991acc4a92616673
ljh3259@naver.com <wlgns12!>
2018-01-16 22:44:12 +0900

    git first push

---
+++ src/main/java/com/ilsan/myggumi/web/controller/SampleController.java
@@ -0,0 +1,15 @@
+package com.ilsan.myggumi.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SampleController {
+
+        @RequestMapping("/")
+        public String helloWorld() {
+            return "hello";
+        }
+
+
+}
