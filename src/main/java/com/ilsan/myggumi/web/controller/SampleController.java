692def47f8c7022f52f8f2e7871af25c0c1d6a8e
sujin <sujin@DESKTOP-SNFSF3U>
2018-02-03 12:45:16 +0900

    타일즈 기본 작업 java파일

--- src/main/java/com/ilsan/myggumi/web/controller/SampleController.java
+++ src/main/java/com/ilsan/myggumi/web/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.ilsan.myggumi.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class SampleController {
@@ -11,8 +12,14 @@ public class SampleController {
         return "index";
     }
 
-    @RequestMapping("/join")
+    @RequestMapping(value="/join", method=RequestMethod.GET)
     public String join() {
         return "user/join";
+    }
+    
+    @RequestMapping(value="/main",method=RequestMethod.GET)
+    public String main() {
+    	
+    	return "layout.index";
     }
 }
