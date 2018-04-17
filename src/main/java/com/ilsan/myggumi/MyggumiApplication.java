d2bf6bba1336365a185c93929ecea6ddfb67f0c9
soop654 <soop654@naver.com>
2018-04-17 15:10:01 +0900

    부트 외부톰캣

--- src/main/java/com/ilsan/myggumi/MyggumiApplication.java
+++ src/main/java/com/ilsan/myggumi/MyggumiApplication.java
@@ -1,11 +1,23 @@
 package com.ilsan.myggumi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class MyggumiApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class MyggumiApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyggumiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MyggumiApplication.class, args);
 	}
